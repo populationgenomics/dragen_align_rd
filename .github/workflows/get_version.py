514e2db3c2f502bea9252f2ab91d0e6eeacadd94
MattWellie <mattwellie@gmail.com>
2025-12-05 10:32:53 +1000

    wf patches

--- .github/workflows/get_version.py
+++ .github/workflows/get_version.py
@@ -46,7 +46,12 @@ def get_next_version_tag(folder: str, version: str) -> str:
             full_image_name,
             '--format=json',
         ]
-        result = subprocess.run(cmd, capture_output=True, text=True, check=True)  # noqa: S603
+
+        try:
+            result = subprocess.run(cmd, capture_output=True, text=True, check=True)  # noqa: S603
+        except subprocess.CalledProcessError:
+            logging.error(f'Failed to list tags for {full_image_name}')
+            continue
 
         # If existing tags are found, proceed to determine the next version.
         tags_list += json.loads(result.stdout)
