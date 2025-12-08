9893f7780356115fccaf6372864ec8e1067e04af
MattWellie <mattwellie@gmail.com>
2025-12-08 16:48:34 +1000

    upload fasta reference

--- src/dragen_align_rd/upload_reference.py
+++ src/dragen_align_rd/upload_reference.py
@@ -1,5 +1,4 @@
 import os
-from icasdk.model.create_data import CreateData
 from icasdk.apis.tags import project_data_api
 
 from dragen_align_rd.utils import run_subprocess_with_log
@@ -11,7 +10,8 @@ BATCH_TMP = os.environ.get('BATCH_TMPDIR', '/io')
 
 
 FASTA = 'gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_assembly38_masked.fasta'
-LOCAL_NAME = f'{BATCH_TMP}/Homo_sapiens_assembly38_masked.cram'
+LOCAL_NAME = f'{BATCH_TMP}/Homo_sapiens_assembly38_masked.fasta'
+LOCAL_INDEX = f'{BATCH_TMP}/Homo_sapiens_assembly38_masked.fasta.fai'
 
 project_id = get_ica_secrets()['projectID']
 
@@ -19,30 +19,18 @@ with get_ica_api_client() as api_client:
 
     pd_api = project_data_api.ProjectDataApi(api_client)
 
-    folder = f'/ref_data/'
-    body = CreateData(
-        name='Homo_sapiens_assembly38_masked.fasta',
-        folderPath=folder,
-        dataType='FILE',
-    )
-    api_response = pd_api.create_data_in_project(  # type: ignore[ReportUnknownVariableType]
-        path_params={'projectId': project_id},  # type: ignore[ReportUnknownVariableType]
-        body=body,
-    )
-
-    new_object_id = api_response.body['data']['id']  # type: ignore[ReportUnknownVariableType]
-    new_status = api_response.body['data']['details']['status']  # type: ignore[ReportUnknownVariableType]
-
     authenticate_ica_cli()
 
     run_subprocess_with_log(['gcloud', 'storage', 'cp', FASTA, LOCAL_NAME], f'Download {FASTA}')
+    run_subprocess_with_log(['gcloud', 'storage', 'cp', f'{FASTA}.fai', LOCAL_INDEX], f'Download {FASTA}')
+    run_subprocess_with_log(['tar', '-cf', 'Homo_sapiens_assembly38_masked.tar', LOCAL_NAME, LOCAL_INDEX], 'TAR files')
     run_subprocess_with_log(
         [
             'icav2',
             'projectdata',
             'upload',
-            LOCAL_NAME,
-            folder,
+            'Homo_sapiens_assembly38_masked.tar',
+            '/ref_data/',
         ],
-        f'Upload {LOCAL_NAME} to ICA',
+        'Upload Reference to ICA',
     )
